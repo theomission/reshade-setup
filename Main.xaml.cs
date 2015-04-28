725d484b5e6f746e0410921442df18355c4d1d2e
Crosire <crosiredev@gmail.com>
2015-04-28 16:18:13 +0200

    Removed Microsoft Visual C++ redistributable check again

--- Main.xaml.cs
+++ Main.xaml.cs
@@ -9,21 +9,6 @@ namespace ReShade.Setup
 	{
 		private void OnStartup(object sender, StartupEventArgs e)
 		{
-			bool isRedistributableInstalled = File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "msvcp110.dll"));
-
-			if (Environment.Is64BitOperatingSystem)
-			{
-				isRedistributableInstalled = isRedistributableInstalled && File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.SystemX86), "msvcp110.dll"));
-			}
-
-			if (!isRedistributableInstalled)
-			{
-				MessageBox.Show("Unable to find the Microsoft Visual C++ 2012 Redistributable on your system. Please install " + (Environment.Is64BitOperatingSystem ? "both the x86 and x64" : "the x86") + " version first!", "Missing Visual C++ Redistributable", MessageBoxButton.OK, MessageBoxImage.Error);
-
-				Shutdown();
-				return;
-			}
-
 			Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
 		}
 	}
