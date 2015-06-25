b5163e2270b6aa41870ba5f9538c2ace3e97567d
Crosire <crosiredev@gmail.com>
2015-06-25 12:13:41 +0200

    Changed target framework version to 4.0

--- Wizard.xaml.cs
+++ Wizard.xaml.cs
@@ -33,7 +33,7 @@ namespace ReShade.Setup
 
 			if (args.Length == 2 && File.Exists(args[1]))
 			{
-				new Thread(delegate() { Install(args[1]); }).Start();
+				new Thread(() => { Install(args[1]); }).Start();
 			}
 		}
 		private void OnClosing(object sender, CancelEventArgs e)
@@ -65,7 +65,7 @@ namespace ReShade.Setup
 
 			if (result.HasValue && result.Value)
 			{
-				new Thread(delegate() { Install(dlg.FileName); }).Start();
+				new Thread(() => { Install(dlg.FileName); }).Start();
 			}
 		}
 		private void OnApiChecked(object sender, RoutedEventArgs e)
@@ -78,7 +78,7 @@ namespace ReShade.Setup
 			FileVersionInfo info = FileVersionInfo.GetVersionInfo(path);
 			string name = !String.IsNullOrEmpty(info.ProductName) ? info.ProductName : Path.GetFileNameWithoutExtension(path);
 	
-			this.Dispatcher.Invoke(delegate()
+			this.Dispatcher.Invoke(new Action(() =>
 			{
 				this.mTargetPath = path;
 				this.Title = "Installing to " + name + " ...";
@@ -86,7 +86,7 @@ namespace ReShade.Setup
 				this.Message.Content = "Analyzing " + name + " ...";
 				this.Progress.Visibility = Visibility.Visible;
 				this.Progress.IsIndeterminate = true;
-			});
+			}));
 
 			#region Analyze Game
 			PEInfo exeInfo = new PEInfo(path);
@@ -100,17 +100,17 @@ namespace ReShade.Setup
 
 			if (nameModule == null)
 			{
-				this.Dispatcher.Invoke(delegate()
+				this.Dispatcher.Invoke(new Action(() =>
 				{
 					this.Title = "Warning!";
 					this.Message.Content = "Auto-detection failed. Please select:";
 					this.Progress.Visibility = Visibility.Collapsed;
 					this.ApiGroup.IsEnabled = true;
-				});
+				}));
 
 				this.mApiCondition.Wait();
 
-				this.Dispatcher.Invoke(delegate()
+				this.Dispatcher.Invoke(new Action(() =>
 				{
 					if (this.ApiDirect3D8.IsChecked.Value)
 					{
@@ -130,7 +130,7 @@ namespace ReShade.Setup
 					}
 
 					this.ApiGroup.IsEnabled = false;
-				});
+				}));
 			}
 
 			if (nameModule == null || this.mFinished)
@@ -138,7 +138,7 @@ namespace ReShade.Setup
 				return;
 			}
 
-			this.Dispatcher.Invoke(delegate()
+			this.Dispatcher.Invoke(new Action(() =>
 			{
 				if (nameModule.StartsWith("d3d8", StringComparison.InvariantCultureIgnoreCase))
 				{
@@ -156,7 +156,7 @@ namespace ReShade.Setup
 				{
 					this.ApiOpenGL.IsChecked = true;
 				}
-			});
+			}));
 			#endregion
 
 			string pathDll = is64Bit ? "ReShade64.dll" : "ReShade32.dll";
@@ -166,13 +166,13 @@ namespace ReShade.Setup
 			{
 				if (MessageBox.Show("Do you want to overwrite the existing installation?", "Existing installation found.", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
 				{
-					this.Dispatcher.Invoke(delegate()
+					this.Dispatcher.Invoke(new Action(() =>
 					{
 						this.mFinished = true;
 						this.Title = "Failed!";
 						this.Message.Content = "Existing installation found.";
 						this.Progress.Visibility = Visibility.Collapsed;
-					});
+					}));
 
 					return;
 				}
@@ -218,13 +218,13 @@ namespace ReShade.Setup
 				string sourcePath = files[i].Item1;
 				string destinationPath = files[i].Item2;
 
-				this.Dispatcher.Invoke(delegate()
+				this.Dispatcher.Invoke(new Action(() =>
 				{
 					this.Message.Content = "Installing \"" + sourcePath + "\" ...";
 					this.Progress.IsIndeterminate = false;
 					this.Progress.Progress = i * 100.0 / files.Count;
 					this.Progress.Visibility = Visibility.Visible;
-				});
+				}));
 
 				try
 				{
@@ -241,27 +241,27 @@ namespace ReShade.Setup
 				}
 				catch (IOException)
 				{
-					this.Dispatcher.Invoke(delegate()
+					this.Dispatcher.Invoke(new Action(() =>
 					{
 						this.mFinished = true;
 						this.Title = "Failed!";
 						this.Message.Content = "Unable to copy file \"" + sourcePath + "\".";
 						this.Progress.Visibility = Visibility.Collapsed;
-					});
+					}));
 
 					return;
 				}
 			}
 			#endregion
 
-			this.Dispatcher.Invoke(delegate()
+			this.Dispatcher.Invoke(new Action(() =>
 			{
 				this.mFinished = true;
 				this.Title = "Success!";
 				this.Button.IsEnabled = true;
 				this.Message.Content = "Run " + name;
 				this.Progress.Visibility = Visibility.Collapsed;
-			});
+			}));
 		}
 	}
 }
